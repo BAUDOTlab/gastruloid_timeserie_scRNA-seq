b11ba705228a54f31b4c1dbfd08a375ed71b994a
celinech <chevalierceline2@gmail.com>
2022-11-08 10:15:19 +0100

    clean up the conversion script

--- scripts/convertSO_to_h5ad.R
+++ scripts/convertSO_to_h5ad.R
@@ -15,20 +15,9 @@ cat("\n-------------------------------------------------------------------------
 cat("Load Seurat Object (SO)\n\n")
 SO <- readRDS(inputFile) 
 
-#' update the Seurat Object to at least v3.1.2
-#cat("Update the SO\n\n")
-#SO_up <- SeuratObject::UpdateSeuratObject(SO)
-
 #' Save as a h5Seurat file format
 cat("Save SO as a h5Seurat file\n\n")
 SeuratDisk::SaveH5Seurat(SO, outputFile)
-
-#' Save update RDS file
-#fileUp <- SeuratDisk::LoadH5Seurat(outputFile)
-#saveRDS(fileUp, file = paste0(outputFile, ".rds"))
-
-#' #' Check the content of the H5Seurat file
-#' SO_H5 <- SeuratDisk::LoadH5Seurat("/mnt/DATA_4TB/projects/gastruloids_scRNAseq/CardiacGastruloidsAnalysis/gatherAnalysis/rdsObjects/08_cellIdentity2LouvainClusters_zaffran3cumulates_alone.h5Seurat")
 
 #' Convert to a h5ad file format, readable by scanpy
 cat("Do the conversion from h5Seurat to h5ad file format\n\n")
