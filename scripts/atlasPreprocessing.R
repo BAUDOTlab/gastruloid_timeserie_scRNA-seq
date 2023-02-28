89e11a913cb1d657b8fd73dd5b8e619d3f66bfb5
celinech <chevalierceline2@gmail.com>
2023-02-28 13:32:11 +0100

    preprocess the Atlas and save the object as an input

---
+++ scripts/atlasPreprocessing.R
@@ -0,0 +1,64 @@
+library(Seurat)
+library(Matrix)
+library(harmony)
+library(cowplot)
+
+
+basePath <- "/mnt/DATA_4TB/projects/gastruloids_sc_Lescroart/analysis/seuratAnalysis"
+inputs.folder <- file.path(basePath, "inputData")
+atlas.folder <- file.path(inputs.folder, "atlas")
+
+
+atlas.metadata <- read.table(file.path(atlas.folder, "meta.tab") , sep="\t" , header=TRUE )
+atlas.genes <- read.csv(file.path(atlas.folder, "genes.tsv"), sep="\t", header = FALSE, as.is = TRUE)
+atlas.data <- readMM(file.path(atlas.folder, "raw_counts.mtx"))
+atlas.data@Dimnames[[2]] <- atlas.metadata$cell
+atlas.data@Dimnames[[1]] <- atlas.genes$V2
+
+rm(atlas.genes)
+gc()
+
+atlas <- CreateSeuratObject(counts = atlas.data, project = "Atlas", min.cells=3, min.features = 200)
+rm(atlas.data)
+gc()
+
+nonFiltered <- intersect(colnames(atlas), atlas.metadata$cell)
+atlas.metadata <- atlas.metadata[which(atlas.metadata$cell %in% nonFiltered),]
+gc()
+
+atlas <- NormalizeData(atlas, normalization.method = "LogNormalize", verbose=FALSE)
+atlas <- AddMetaData(object = atlas, metadata = atlas.metadata$stage, col.name = "day")
+atlas <- AddMetaData(object = atlas, metadata = atlas.metadata$celltype, col.name = "celltype")
+atlas <- AddMetaData(object = atlas, metadata = atlas.metadata$sequencing.batch, col.name = "replicate")
+atlas <- AddMetaData(object = atlas, metadata = paste(atlas$day, "_batch_0", atlas$replicate, sep=""), col.name = "dataset")
+atlas$model <- "Embryos"
+rm(atlas.metadata, nonFiltered)
+gc()
+
+Idents(atlas) <- atlas$day
+atlas.subset <- subset(x = atlas, idents = c("mixed_gastrulation"), invert=T)
+
+Idents(atlas.subset) <- atlas.subset$celltype
+atlas.subset <- subset(x = atlas.subset, cells = colnames(atlas.subset)[is.na(Idents(atlas.subset))], invert = T)
+atlas.subset <- subset(atlas.subset, idents=unique(grep("ExE", atlas.subset$celltype, value = T)), invert=T)
+atlas.subset <- subset(atlas.subset, idents="Parietal endoderm", invert=T)
+rm(atlas)
+gc()
+
+atlas.subset <- FindVariableFeatures(atlas.subset, nfeatures=2000, selection.method = "vst", verbose=FALSE)
+atlas.subset <- ScaleData(atlas.subset, do.scale = FALSE, verbose = FALSE)
+atlas.subset <- RunPCA(atlas.subset, npcs = 50, verbose = FALSE)
+gc()
+
+p1 <- DimPlot(object = atlas.subset, reduction = "pca", pt.size = .1, group.by = "day")
+p2 <- VlnPlot(object = atlas.subset, features = "PC_1", group.by = "day", pt.size = .1)
+plot_grid(p1,p2)
+
+
+saveRDS(atlas.subset, file.path(atlas.folder, paste0("atlas_preprocessed.rds")))
+
+top.pcs <- 30
+general.seed <- 17
+atlas.subset <- RunUMAP(atlas.subset, dims = 1:top.pcs, seed.use = general.seed, verbose = FALSE)
+
+saveRDS(atlas.subset, file.path(atlas.folder, paste0("atlas_preprocessed_UMAP.rds")))
